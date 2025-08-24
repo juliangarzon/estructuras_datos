7fa5d533955e95dbaa3cb19a26081f349c6978f2
julian garzon <julian.e.garzon@gmail.com>
2025-08-24 11:21:42 -0500

    clase 3 solucion

---
+++ clase3/demo/TestArrayList.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class TestArrayList {
+    public static void main(String[] args){
+        ArrayList<String> miLista = new ArrayList<String>();
+        miLista.add("Objeto 1");
+        miLista.add("Objeto 2");
+        miLista.add("Objeto 3");
+        System.out.println(miLista.toString());
+
+    }
+}
